16465752ee3f3ec3e993815c1cd0f1603ea5a717
rubensfig <r.figueiredo.52@gmail.com>
2018-01-24 01:09:15 +0100

    almost

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 class Object {
     public:
-        std::pair<int, int> pos;        
+        std::pair<float, float> pos;        
 };
 
 class Buttons {
@@ -21,8 +21,8 @@ class Buttons {
         }
 
         void init_pos() {
-            int x = 100;
-            int y = 25;
+            float x = 100;
+            float y = 25;
             for (int i=0; i < buttons; i++, y+=25) {
                 Object obj;
                 obj.pos = std::make_pair(x,y);
@@ -34,25 +34,39 @@ class Buttons {
         int buttons = 3;
         sf::RectangleShape but;
 };
+
 class Balls{
     public:
-        std::list<Object> balls;
+        std::list<std::pair<sf::CircleShape, Object>> balls;
+
+        virtual ~Balls() {};
         Balls() {
-            init_pos();
-
-        }
-
-        void init_pos() {
             for (int i=0; i < nballs; ++i) {
                 Object obj;
+                sf::CircleShape shape(30.f);
+                shape.setFillColor(sf::Color::Green);
+                shape.setPosition(30, 30);
+
                 obj.pos = std::make_pair(0,0);
-                balls.emplace_back(obj);
+
+                balls.emplace_back(std::make_pair(shape, obj));
             }
+            update_pos();
         }
 
+        void update_pos() {
+            std::pair<float, float> temp;
+            for (auto &i : balls){
+
+                float x = std::rand() % 900;
+                float y = std::rand() % 730;
+
+                i.second.pos.first = x;
+                i.second.pos.second = y;
+            }
+        }
     private:
         int nballs = 50;
-        sf::CircleShape ball;
 };
 
 class Game {
@@ -66,12 +80,24 @@ class Game {
             state = GM_START;
         }
 
-        void game_handler() {
-            std::cout << "game" << std::endl;
+        virtual ~Game() {};
 
-            for(auto obj: gm_object.balls)
-                std::cout << obj.pos.first << std::endl;
+        void game_handler(const std::pair<float, float>* key_press) {
+            //std::cout << "game" << std::endl
+            gm_object.update_pos();
 
+            auto element = find_closest(key_press);
+
+            return;
+        }
+
+        std::pair<sf::CircleShape, Object> find_closest(const std::pair<float, float>* key_press) {
+            int i = 0;
+            for (auto b: gm_object.balls) {
+                if(b.first.getGlobalBounds().contains(key_press->first, key_press->second)) {
+                    return b;
+                }
+            }
         }
 
         void add_credits() {
@@ -111,26 +137,47 @@ class Game {
         }
 };
 
-void draw_all(Balls balls, Buttons btns, sf::RenderWindow* window) {
-        window->display();
+void draw_all(Balls* balls, Buttons* btns, sf::RenderWindow* window) {
+        
+    window->clear();
+
+    for (auto &it: balls->balls) {
+        it.first.setPosition(it.second.pos.first, it.second.pos.second);
+        window->draw(it.first);
+    }
+
+    window->display();
 }
 
 int main() {
 
     Game loop = Game();
     Buttons btns = Buttons();
-    sf::RenderWindow window(sf::VideoMode(200, 200), "fmq_game");
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "fmq_game");
+    sf::Event event;
+    std::pair<float, float> key_press;
+    bool pressed = false;
 
     while (loop.state != GM_EXIT) {
-        std::cout << loop.state << std::endl;
+        while(window.pollEvent(event)) {
+            if (event.type == sf::Event::MouseButtonPressed)
+                if (event.mouseButton.button == sf::Mouse::Left){
+                    key_press = std::make_pair(event.mouseButton.x, event.mouseButton.y);
+                    pressed = true;
+                }
+        }
         switch(loop.state) {
             case GM_START:
                 loop.main_menu();
                 break;
             case GM_PLAY:
-                loop.game_handler();
+                if (!pressed)
+                    continue;
+
+                loop.game_handler(&key_press);
                 break;
         }
-        draw_all(loop.gm_object, btns, &window);
+        draw_all(&loop.gm_object, &btns, &window);
+        pressed = false;
     }
 }
